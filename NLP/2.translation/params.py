0d1333d3409c372798eb3038679da2fa31bdf501
speecher <hit_master@163.com>
2018-11-26 18:21:03 +0800

    20181126
    仿照nmt，搭建一个轻量级的nmt

---
+++ NLP/2.translation/params.py
@@ -0,0 +1,35 @@
+import tensorflow as tf
+
+
+def create_hparams():
+    params = tf.contrib.training.HParams(
+        num_units = 256, # Network size
+        num_layers = 2, # Network depth
+        attention_type = 'luong', #  luong | bahdanau | None
+        attention_architecture = 'standard', # standard | gnmt
+        optimizer = 'sgd', # sgd , adam
+        learning_rate = 1., # sgd:1, Adam:0.0001
+        decay_steps = 10000,
+        decay_rate = 0.99,
+        epochs = 50,
+        out_dir = 'log/model',
+        # vocab
+        encoder_vocab_size = 50,
+        decoder_vocab_size = 50,
+        share_vocab = False,
+        # Sequence lengths
+        src_max_len = 50,
+        tgt_max_len = 50,
+        # Default settings works well (rarely need to change)
+        unit_type = 'lstm', # lstm | gru | rnn
+        keep_prob = 0.8,
+        max_gradient_norm = 1,
+        batch_size = 8,
+        num_gpus = 1,
+        metrics = None,
+        time_major = True,
+        # inference
+        infer_mode = 'greedy', # greedy | beam_search
+        beam_width = 0,
+        num_translations_per_input = 10)
+    return params
--- NLP/2.translation/utils.py
+++ NLP/2.translation/utils.py
@@ -1,4 +1,38 @@
 import numpy as np
+import tensorflow as tf
+
+
+def attention_mechanism_fn(attention_type, num_units, memory, encoder_length):
+    if attention_type == 'luong':
+        attention_mechanism = tf.contrib.seq2seq.LuongAttention(
+            num_units, memory, memory_sequence_length=encoder_length)
+    elif attention_type == 'bahdanau':
+        attention_mechanism = tf.contrib.seq2seq.BahdanauAttention(
+            num_units, memory, memory_sequence_length=encoder_length)
+    else:
+        raise ValueError('unkown atteion type %s' % attention_type)
+    return attention_mechanism
+
+
+def create_rnn_cell(unit_type, num_units, num_layers, keep_prob):
+    def single_rnn_cell():
+        if unit_type == 'lstm':
+            single_cell = tf.contrib.rnn.LSTMCell(num_units)
+        elif unit_type == 'gru':
+            single_cell = tf.contrib.rnn.GRUCell(num_units)
+        elif unit_type == 'rnn':
+            single_cell = tf.contrib.rnn.LSTMCell(num_units)
+        else:
+            raise ValueError("Unknown cell type %s" % unit_type)
+        cell = tf.contrib.rnn.DropoutWrapper(
+            single_cell,
+            output_keep_prob=keep_prob)
+        return cell
+    mul_cell = tf. contrib.rnn.MultiRNNCell(
+        [single_rnn_cell() for _ in range(num_layers)])
+    return mul_cell
+
+
 
 class GenData():
 	"""docstring for GenData"""
@@ -7,7 +41,7 @@ class GenData():
 		super(GenData, self).__init__()
 		# 特殊字符
 		self.SOURCE_CODES = ['<PAD>', '<UNK>']
-		self.TARGET_CODES = ['<PAD>', '<EOS>', '<UNK>', '<GO>']  # 在target中，需要增加<GO>与<EOS>特殊字符
+		self.TARGET_CODES = ['<PAD>', '<GO>', '<EOS>', '<UNK>']  # 在target中，需要增加<GO>与<EOS>特殊字符
 		self.filepath = filepath
 		self._init_data()
 		self._init_vocab()
@@ -18,19 +52,19 @@ class GenData():
 		# ========读取原始数据========
 		with open(self.filepath, 'r', encoding='utf-8') as f:
 		    data = f.read()
-		self.data_list = data.split('\n')[:1500]
+		self.data_list = data.split('\n')[:500]
 		self.input_list = [line.split('\t')[0] for line in self.data_list]
 		self.output_list = [line.split('\t')[1] for line in self.data_list]
 
 
 	def _init_vocab(self):
 		# 生成输入字典
-		self.input_vocab = set(''.join(self.input_list))
+		self.input_vocab = sorted(list(set(''.join(self.input_list))))
 		self.id2inp = self.SOURCE_CODES + list(self.input_vocab)
 		self.inp2id = {c:i for i,c in enumerate(self.id2inp)}
 		# 输出字典
-		self.output_vocab = set(''.join(self.output_list))
-		self.id2out = self.TARGET_CODES + list(self.output_vocab)
+		self.output_vocab = sorted(list(set(''.join(self.output_list))))
+		self.id2out = self.TARGET_CODES + self.output_vocab
 		self.out2id = {c:i for i,c in enumerate(self.id2out)}
 
 
@@ -49,7 +83,7 @@ class GenData():
 	        encoder_inputs = self.en_inp_num_data[begin:end]
 	        decoder_inputs = self.de_inp_num[begin:end]
 	        decoder_targets = self.de_out_num[begin:end]
-	        encoder_lengths = [len(line) for line in encoder_inputs]        
+	        encoder_lengths = [len(line) for line in encoder_inputs]
 	        decoder_lengths = [len(line) for line in decoder_inputs]
 	        encoder_max_length = max(encoder_lengths)
 	        decoder_max_length = max(decoder_lengths)
@@ -59,8 +93,3 @@ class GenData():
 	        mask = decoder_targets > 0
 	        target_weights = mask.astype(np.int32)
 	        yield encoder_inputs, decoder_inputs, decoder_targets, target_weights, encoder_lengths, decoder_lengths
-	              
-
-
-
-
